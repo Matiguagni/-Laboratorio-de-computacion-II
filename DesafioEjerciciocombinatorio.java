ffc91fcdecf9ed4c416175d3df549615237f00bb
Matias Guagni <85851958+Matiguagni@users.noreply.github.com>
2021-09-02 17:34:27 -0300

    Add files via upload

---
+++ DesafioEjerciciocombinatorio.java
@@ -0,0 +1,8 @@
+public class DesafioEjerciciocombinatorio {
+
+    public static void main(String[] args) {
+	// write your code here
+        double ejercicio = (((6 / 5) - (2 / 3) * (7 / 2) + (2 / 30)) / (((1.0 / 3.0) / 5)));
+        System.out.println("El resultado de la operacion combinada es de = " + ejercicio);
+    }
+}
