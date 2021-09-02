88e5afd3ecaa7560c3ed6c2344a3b975fc796e9a
Matias Guagni <85851958+Matiguagni@users.noreply.github.com>
2021-09-02 17:32:22 -0300

    Delete Combinatoria.java

--- Combinatoria.java
+++
@@ -1,8 +0,0 @@
-public class Combinatoria {
-
-    public static void main(String[] args) {
-	// write your code here
-        double ejercicio = (((6 / 5) - (2 / 3) * (7 / 2) + (2 / 30)) / (((1.0 / 3.0) / 5)));
-        System.out.println("El resultado de la operacion combinada es de = " + ejercicio);
-    }
-}
